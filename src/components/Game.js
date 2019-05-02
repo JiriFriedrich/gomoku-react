e65bd84786aead46bf1dbd6be7b9ced0f5754fff
Jiri Friedrich <jiri.friedrich@icloud.com>
2019-05-02 07:25:43 +0200

    Transfer class components to functional

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,107 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react'
 import Board from './Board'
+import {calculateWinner} from '../lib/lib'
 
-class Game extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			history: [
-				{
-					squares: Array(9).fill(null)
-				}
-			],
-			stepNumber: 0,
-			xIsNext: true
-		};
-	}
+const Game = () => {
+	const [history, setHistory] = useState([{
+		squares: Array(9).fill(null)
+	}])
 
-	handleClick(i) {
-		const history = this.state.history.slice(0, this.state.stepNumber + 1);
-		const current = history[history.length - 1];
+	const [stepNumber, setStepNumber] = useState(0)
+
+	const [xIsNext, setXIsNext] = useState(true)
+
+	const handleClick = (i) => {
+		const updatedHistory = history.slice(0, stepNumber + 1);
+		const current = updatedHistory[updatedHistory.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
 			return;
 		}
-		squares[i] = this.state.xIsNext ? "X" : "O";
-		this.setState({
-			history: history.concat([
-				{
-					squares: squares
-				}
-			]),
-			stepNumber: history.length,
-			xIsNext: !this.state.xIsNext
-		});
+		squares[i] = xIsNext ? "X" : "O";
+		setHistory(updatedHistory.concat([{squares: squares}]))
+		setStepNumber(updatedHistory.length)
+		setXIsNext(!xIsNext)
 	}
 
-	jumpTo(step) {
-		this.setState({
-			stepNumber: step,
-			xIsNext: (step % 2) === 0
-		});
+	const jumpTo = (step) => {
+		setStepNumber(step)
+		setXIsNext((step % 2) === 0)
 	}
 
-	render() {
-		const history = this.state.history;
-		const current = history[this.state.stepNumber];
-		const winner = calculateWinner(current.squares);
+	const current = history[stepNumber];
+	const winner = calculateWinner(current.squares);
 
-		const moves = history.map((step, move) => {
-			const desc = move ?
-				'Go to move #' + move :
-				'Go to game start';
-			return (
-				<li key={move}>
-					<button onClick={() => this.jumpTo(move)}>{desc}</button>
-				</li>
-			);
-		});
-
-		let status;
-		if (winner) {
-			status = "Winner: " + winner;
-		} else {
-			status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-			if (!current.squares.includes(null)) {
-				status = "Draw!"
-			}
-		}
-
+	const moves = history.map((step, move) => {
+		const desc = move ?
+			'Go to move #' + move :
+			'Go to game start';
 		return (
-			<div className="game">
-				<div className="game-board">
-					<Board
-						squares={current.squares}
-						onClick={i => this.handleClick(i)}
-					/>
-				</div>
-				<div className="game-info">
-					<div>{status}</div>
-					<ol>{moves}</ol>
-				</div>
-			</div>
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{desc}</button>
+			</li>
 		);
-	}
-}
+	});
 
-function calculateWinner(squares) {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6]
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
+	let status;
+	if (winner) {
+		status = "Winner: " + winner;
+	} else {
+		status = "Next player: " + (xIsNext ? "X" : "O");
+		if (!current.squares.includes(null)) {
+			status = "Draw!"
 		}
 	}
-	return null;
+
+	return (
+		<div className="game">
+			<div className="game-board">
+				<Board
+					squares={current.squares}
+					onClick={i => handleClick(i)}
+				/>
+			</div>
+			<div className="game-info">
+				<div>{status}</div>
+				<ol>{moves}</ol>
+			</div>
+		</div>
+	);
 }
 
 export default Game;
